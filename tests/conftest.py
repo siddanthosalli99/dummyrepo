53e75a0d59ae48d01dec3300f75bb757d14e8973
siddanthosalli99 <siddanthosalli99@gmail.com>
2026-08-16 00:46:28 +0530

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,12 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app import app
+from database.connection import Base, engine
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database() -> None:
+    Base.metadata.create_all(bind=engine)
 
 
 @pytest.fixture
